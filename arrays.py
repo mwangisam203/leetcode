145804fec4ed6c78590d5ff40fec4de957b745d1
mwangisam203 <mwangisam203@gmail.com>
2026-07-14 18:01:13 -0400

    target

--- arrays.py
+++ arrays.py
@@ -8,3 +8,15 @@ def contains_duplicate(nums):
 
 print(contains_duplicate([1, 2, 3, 1]))  # True
 print(contains_duplicate([1, 2, 3, 4]))  # False
+
+
+##Given an array and a target, return the indices of two numbers that add up to the target.
+def two_sum(nums, target):
+    seen = {}  # value -> index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+print(two_sum([2, 7, 11, 15], 9))  # [0, 1]
