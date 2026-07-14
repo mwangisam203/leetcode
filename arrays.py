47c55f1e4b407ddf629aaee6e64e6f97ed532c6e
mwangisam203 <mwangisam203@gmail.com>
2026-07-14 18:14:57 -0400

    mod arr

--- arrays.py
+++ arrays.py
@@ -62,4 +62,20 @@ print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
 ##5: Product of Array Except Self
 ##Problem: Return an array where each element is the product of all other elements (no division allowed).
 
+def product_except_self(nums):
+    n = len(nums)
+    result = [1] * n
 
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
+
+print(product_except_self([1, 2, 3, 4]))  # [24, 12, 8, 6]
