a19905acc08c03d9249b8fc7dffad702f6a379d1
mwangisam203 <mwangisam203@gmail.com>
2026-07-15 17:33:12 -0400

    some more practice

--- Notes.py
+++ Notes.py
@@ -6,3 +6,12 @@
 #Sorting when order doesn't matter but grouping does (e.g., anagrams)
 #Basic array traversal and prefix/suffix accumulation (running totals as you loop)
 
+my_array = [12, 14, 7, 20, 9, 3, 34, 0]
+
+minArray = my_array[0]
+
+for i in my_array:
+
+    if i < minArray:
+        minArray = i
+print("my small number:", minArray)
