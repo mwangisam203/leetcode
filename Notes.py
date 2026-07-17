fb09318875d1e1d0fa0feeabc59f66247ed1ca72
mwangisam203 <mwangisam203@gmail.com>
2026-07-17 19:02:36 -0400

    Big O notes

--- Notes.py
+++ Notes.py
@@ -6,4 +6,11 @@
 #Sorting when order doesn't matter but grouping does (e.g., anagrams)
 #Basic array traversal and prefix/suffix accumulation (running totals as you loop)
 
-
+'''
+| Input (n) | O(1) | O(log n) |      O(n) |  O(n log n) |             O(n²) |
+| --------: | ---: | -------: | --------: | ----------: | ----------------: |
+|        10 |    1 |       ~3 |        10 |         ~33 |               100 |
+|       100 |    1 |       ~7 |       100 |        ~664 |            10,000 |
+|     1,000 |    1 |      ~10 |     1,000 |      ~9,966 |         1,000,000 |
+| 1,000,000 |    1 |      ~20 | 1,000,000 | ~19,931,000 | 1,000,000,000,000 |
+'''
