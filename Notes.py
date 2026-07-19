f7f7f46b17113da9fea2d7bd2d057d364617d280
mwangisam203 <mwangisam203@gmail.com>
2026-07-19 19:10:18 -0400

    sample 5

--- Notes.py
+++ Notes.py
@@ -29,3 +29,20 @@ def find_min_by_key(items, key):
 # example: list of (name, score) tuples, min by score
 people = [("A", 85), ("B", 72), ("C", 90)]
 lowest = find_min_by_key(people, key=lambda x: x[1])
+
+from collections import deque
+
+
+
+def sliding_window_min(arr, k):
+    result = []
+    dq = deque()  # stores indices, increasing value order
+    for i, num in enumerate(arr):
+        while dq and arr[dq[-1]] >= num:
+            dq.pop()
+        dq.append(i)
+        if dq[0] <= i - k:
+            dq.popleft()
+        if i >= k - 1:
+            result.append(arr[dq[0]])
+    return result
