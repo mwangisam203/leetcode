e4923b115d53e529a5ceb038893db7c820b952c3
mwangisam203 <mwangisam203@gmail.com>
2026-07-28 14:12:12 -0400

    block

--- test.py
+++ test.py
@@ -26,4 +26,13 @@ def itsAnagram(a, b):
     count = {}
 
     for char in a:
-        count[char]
+        count[char] = count.get(char, 0) + 1
+
+    for char in b:
+        if char not in count or count[char] == 0:
+            return False
+        count[char] -= 1
+
+    return True
+
+print(itsAnagram("sam", "mam"))
