84992f00eb117d7631752d9f3b5f9705b9501021
mwangisam203 <mwangisam203@gmail.com>
2026-07-31 05:23:26 -0400

    test

--- test.py
+++ test.py
@@ -13,3 +13,25 @@ def validAnagram(t, s):
 
     return True
 print(validAnagram("jess", "jess"))
+
+my_cart = [23, 9, 13, 6, 18, 1, 25, 2]
+
+smallest_number = my_cart[0]
+
+for num in my_cart:
+    if num < smallest_number:
+        smallest_number = num
+print(smallest_number)
+
+
+def minVal(myArray):
+    myArray = [20, 23, 14, 37, 4]
+
+    minVal = myArray[0]
+
+    for i in myArray:
+        if i < minVal:
+            return minVal 
+
+
+print(minVal)
