ccee523fbd4bbb68b9673df9194afca4ac56b807
mwangisam203 <mwangisam203@gmail.com>
2026-07-31 11:02:00 -0400

    testing

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -363,3 +363,39 @@ class Answer:
 # print(Ans)
 
 print(Answer().largest([8,4,10,2]))
+
+
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -= 1
+        return True
+
+print(Solution().isAnagram("jar", "jam"))
+
+
+
+class Solution:
+    def itsAnagram(self, t, s):
+        if len(t) != len(s):
+            return False
+
+        count = {}
+        for char in t:
+            count[char] = count.get(char, 0) + 1
+        for char in s:
+            if char not in count or count[char] == 0:
+                return False
+        return True
+
+print(Solution().itsAnagram("car", "bar"))
