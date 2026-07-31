877d40f9af2083ef2eee0dbce8e57f771b85b7d4
mwangisam203 <mwangisam203@gmail.com>
2026-07-31 11:13:37 -0400

    testing anagram

--- Hashing/hashing.py
+++ Hashing/hashing.py
@@ -41,6 +41,18 @@ def itsAnagram(a, b):
 
 print(itsAnagram("sam", "mam"))
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
 
 
 ## Brute force approach 1 (sorting:)
