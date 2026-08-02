6853bda6c632ed6547099c22370c5fd23ba99070
mwangisam203 <mwangisam203@gmail.com>
2026-08-02 12:40:34 -0400

    some edits

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -56,23 +56,6 @@ but the hash set approach is still faster overall since it avoids the log n fact
 
 
 
-
-#Two Sum
-
-##Given an array and a target, return the indices of two numbers that add up to the target.
-
-def two_sum(nums, target):
-    seen = {}  # value -> index
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
-
-print(two_sum([2, 7, 11, 15], 9))  # [0, 1]
-
-
-
 #Valid Anagram
 #Problem: Given two strings, check if one is a rearrangement of the other.
 
