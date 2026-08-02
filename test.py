dbd7182307d53f67ed0e9ff211c8c1a9c319317b
mwangisam203 <mwangisam203@gmail.com>
2026-08-02 13:30:48 -0400

    prac bench edits

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
+## two_summ
 
+class Solution:
+    def two_summ(self, nums, target):
+
+        seen = {}
+        for i, num in enumerate(nums):
+            value = target - num
+            if value in seen:
+                return [seen[value], i]
+            seen[num] = i
+        return False
+
+
+print(Solution().two_summ([2, 7, 9, 11], target=9))
+
+
+
+            
 
