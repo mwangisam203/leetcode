c4e9206376937c87ca0ec1f79a4009991316f750
mwangisam203 <mwangisam203@gmail.com>
2026-08-03 04:05:28 -0400

    prac bench edits

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-def validAnagram(s, t):
+def Anagram(s, t):
     if len(s) != len(t):
         return False
 
-    count = {}
+    CountS = {}
+    CountT = {}
 
-    for char in s:
-        count[char] = count.get(char, 0) + 1
-    for char in t:
-        if char not in count or count[char] == 0:
-            return False
-        count[char] -= 1
-    return True
-    
-print(validAnagram("race", "care"))
+    for i in range (len(s)):
+        CountS[s[i]] = 1 + CountS.get(s[i], 0)
+        CountT[t[i]] = 1 + CountT.get(t[i], 0)
+    return CountS == CountT
 
-
-
-
-            
-
+print(Anagram("team", "meat"))
