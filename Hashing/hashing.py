7c3e9a9795499f2afc2ad172b2380f099712cc3d
mwangisam203 <mwangisam203@gmail.com>
2026-08-04 08:08:07 -0400

    edits

--- Hashing/hashing.py
+++ Hashing/hashing.py
@@ -1,5 +1,38 @@
 ##valid anagram
 
+def Anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+    for char in t:
+        if char not in count or count[char] == 0:
+            return False
+        count[char] -= 1
+    return True
+
+
+print(Anagram("eat", "ete"))
+
+
+def my_anagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    CountS, CountT = {}, {}
+    for i in range(len(s)):
+        CountS[s[i]] = 1 + CountS.get(s[i], 0)
+        CountT[t[i]] = 1 + CountT.get(t[i], 0)
+    return CountS == CountT
+
+
+print(my_anagram("eat", "ete"))
+
+
+
+
 def validAnagram(s, t):
 
     if len(s) != len(t):
