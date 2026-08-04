78833e50132931ea340edc60da3101e2f2bc7328
mwangisam203 <mwangisam203@gmail.com>
2026-08-04 12:26:08 -0400

    edits

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from collections import defaultdict
 
 class Solution:
-    def groupAnagram(str):
+    def groupAnagram(strs):
         result = defaultdict(list)
-        
+
+        for s in strs:
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                
+
