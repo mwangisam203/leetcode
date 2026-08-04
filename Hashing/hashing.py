cc2a73745b8322ec76ea9746b752a5900e59f754
mwangisam203 <mwangisam203@gmail.com>
2026-08-04 08:06:04 -0400

    edits

--- Hashing/hashing.py
+++ Hashing/hashing.py
@@ -88,6 +88,21 @@ class Solution:
 
 print(Solution().two_summ([2, 7, 9, 11], target=9))
 
+
+
+def twosum(nums, target):
+
+    viewed = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in viewed:
+            return [viewed[complement], i]
+        viewed[num] = i
+
+
+print(twosum([2, 7, 8, 11,15], target=9))
+
 ## Brute force approach 1 (sorting:)
 def isAnagram(s, t):
     if len(s) != len(t):
