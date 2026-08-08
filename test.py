fc6ef23bcc9320b438f81ed9bce1cfbe9a959f7c
mwangisam203 <mwangisam203@gmail.com>
2026-08-08 03:02:56 -0400

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def groupAnagram(strs):
+    res = defaultdict(list)
+
+    for s in strs:
+        count = 0 * 26
+
+        for c in s:
+            ord[c] - ord["a"]
+        res  
