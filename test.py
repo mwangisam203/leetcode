f4104f19c968365e1c248851aad5c6e6160196e2
mwangisam203 <mwangisam203@gmail.com>
2026-08-08 03:17:02 -0400

    edits

--- test.py
+++ test.py
@@ -10,3 +10,29 @@ def groupAnagram(strs):
             ord[c] - ord["a"] += 1
         res[tuple(count)].append(s)
     return list(res.values())
+
+
+
+def contdupli(nums):
+
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+def newAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    count = {}
+    for char in s:
+        count[char] = count.get(char, 0) + 1
+    for char in t:
+        if char not in count or count[char] == 0:
+            return False
+        count[char] -= 1
+    return True
