35b52ec0b811aaa263f765978480dc7d851d1ae1
mwangisam203 <mwangisam203@gmail.com>
2026-08-09 05:18:12 -0400

    some hard task

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+from collections import defaultdict
 
+def groupAnagram(strs):
+    res = defaultdict(list)
+
+    for s in strs:
+        count = 0 * 26 # a .. z
+
+        for c in s:
+            ord(c) - ord("a") += 1
+        res[tuple(count)].append(s)
+    return list(res.values())
+
+
+print(groupAnagram(["act","pots","tops","cat","stop","hat"]))
