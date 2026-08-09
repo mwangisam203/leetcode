c162dd98abf558ff7851d1dada6a3bcde5023275
mwangisam203 <mwangisam203@gmail.com>
2026-08-09 05:25:52 -0400

    edits

--- test.py
+++ test.py
@@ -3,13 +3,30 @@ from collections import defaultdict
 def groupAnagram(strs):
     res = defaultdict(list)
 
-    for s in strs:
-        count = 0 * 26 # a .. z
+    #res = defaultdict(list)
 
-        for c in s:
-            ord(c) - ord("a") += 1
-        res[tuple(count)].append(s)
+    for s in strs:
+            # counting freq of each string
+        Count = [0] * 26
+            
+        for char in s: 
+            Count[ord(char) - ord('a')] += 1
+        res[tuple(Count)].append(s)
     return list(res.values())
 
 
 print(groupAnagram(["act","pots","tops","cat","stop","hat"]))
+
+
+
+
+# res = defaultdict(list)
+
+#         for s in strs:
+#             # counting freq of each string
+#             Count = [0] * 26
+            
+#             for char in s: 
+#                 Count[ord(char) - ord('a')] += 1
+#             res[tuple(Count)].append(s)
+#         return list(res.values())
