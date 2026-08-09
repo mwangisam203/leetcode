1d89249e58d8ad9a58c42776b3124ba088211d40
mwangisam203 <mwangisam203@gmail.com>
2026-08-09 05:48:23 -0400

    edits

---
+++ test.py
@@ -0,0 +1,15 @@
+
+from collections import defaultdict
+
+def groupanagram(strs):
+
+    res = defaultdict(list)
+
+    for word in strs:
+        count = [0] * 26
+
+        for char in word:
+            count[ord(char) - ord("a")] += 1
+        res[tuple(count)].append(word)
+
+    return list(res.values())
