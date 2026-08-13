ee7921ddf31425e45ca17be2365463bbfdbe65a1
mwangisam203 <mwangisam203@gmail.com>
2026-08-13 15:06:02 -0400

    edits

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 
 
 
+def hasduplicates(words):
+
+    seen = set()
+
+    for word in words:
+        if word in seen:
+            return True
+        seen.add(word)
+    return False
+
 
         
