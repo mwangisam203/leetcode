1a55bb73640932ec9dda9d8d4542f6fdb5b0dd35
mwangisam203 <mwangisam203@gmail.com>
2026-08-14 16:25:24 -0400

    some del

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -378,17 +378,3 @@ print(Solution().isAnagram("jar", "jam"))
 
 
 
-class Solution:
-    def itsAnagram(self, t, s):
-        if len(t) != len(s):
-            return False
-
-        count = {}
-        for char in t:
-            count[char] = count.get(char, 0) + 1
-        for char in s:
-            if char not in count or count[char] == 0:
-                return False
-        return True
-
-print(Solution().itsAnagram("car", "bar"))
