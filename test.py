28ad1b090b403e0e24f93c9d403f8549acf2e427
mwangisam203 <mwangisam203@gmail.com>
2026-08-15 16:10:05 -0400

    some del

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-class Solution:
-    def hasDuplicate(self, nums: list[int]) -> bool:
 
-        nums.sort()
-        for i in range (1, len(nums)):
-            if nums[i] == nums[i - 1]:
-                return True
-        return False
-
-solution = Solution()
-print(solution.hasDuplicate([1,4,3]))
 
