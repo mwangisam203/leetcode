71ce83cc10bc4aac7f268b4856d4faf865d8c3b6
mwangisam203 <mwangisam203@gmail.com>
2026-09-02 16:36:38 -0400

    some test

--- test.py
+++ test.py
@@ -18,3 +18,24 @@ def isAnagram(s, t):
     return sorted(s) == sorted(t)
 
 print(isAnagram("adc", "dac"))    
+
+
+
+def itsAnagram(a, b):
+
+    if len(a) != len(b):
+        return False
+
+    count = {}
+
+    for char in a:
+        count[char] = count.get(char, 0) + 1
+
+    for char in b:
+        if char not in count or count[char] == 0:
+            return False
+        count[char] -= 1
+
+    return True
+
+print(itsAnagram("sam", "mam"))
