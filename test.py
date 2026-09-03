c03c4eb509234aea4bdeb3a261a4453b862c7a73
mwangisam203 <mwangisam203@gmail.com>
2026-09-03 16:00:19 -0400

    some test code and delete after

--- test.py
+++ test.py
@@ -20,22 +20,3 @@ def isAnagram(s, t):
 print(isAnagram("adc", "dac"))    
 
 
-
-def itsAnagram(a, b):
-
-    if len(a) != len(b):
-        return False
-
-    count = {}
-
-    for char in a:
-        count[char] = count.get(char, 0) + 1
-
-    for char in b:
-        if char not in count or count[char] == 0:
-            return False
-        count[char] -= 1
-
-    return True
-
-print(itsAnagram("sam", "mam"))
