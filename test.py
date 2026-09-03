2fd63768ca2f068cbcc09c1b85cfc99b06d4c809
mwangisam203 <mwangisam203@gmail.com>
2026-09-03 16:01:13 -0400

    some deleted code

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 def twosum(nums, target):
 
-    viewed = {}
-
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in viewed:
-            return [viewed[complement], i]
-        viewed[num] = i
-
-
-print(twosum([2, 7, 8, 11,15], target=9))
 
 
 
