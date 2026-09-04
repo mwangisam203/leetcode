a99850aea341b890b3611a1baf7553b2cd03584b
mwangisam203 <mwangisam203@gmail.com>
2026-09-04 15:08:52 -0400

    some deleted code

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+def two_sum(nums, target):
+    seen = {}  # value -> index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+print(two_sum([2, 7, 11, 15], 9))  # [0, 1]
 
 
 
